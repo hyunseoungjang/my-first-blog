f51a7b5bcf227f0385834fdb457ee74db4958d9e
hyunseoungjang <hyunseoungjang@gmail.com>
2015-12-18 23:07:12 +0900

    PythonDjango, 힘들었다

--- mysite/settings.py
+++ mysite/settings.py
@@ -84,7 +84,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
-
+#STATIC_ROOT =  os.path.join(BASE_DIR, "static")
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
     '/static/',
